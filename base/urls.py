17fa397588d620c1b53408dcd8f5067b2553c6be
LrnzDC3696 <lrnzdc3696+github@gmail.com>
2024-06-07 12:33:34 +0800

    created views and urls

--- base/urls.py
+++ base/urls.py
@@ -1,6 +1,32 @@
 from django.urls import path
-from .views import hello_world
+from .views import (
+    hello_world,
+    ExpenseCategoryList,
+    ExpenseCategoryDetail,
+    IncomeCategoryList,
+    IncomeCategoryDetail,
+    AccountList,
+    AccountDetail,
+    TransferList,
+    TransferDetail,
+    ExpenseList,
+    ExpenseDetail,
+    IncomeList,
+    IncomeDetail,
+)
 
 urlpatterns = [
     path("", hello_world, name="base_hello-world"),
+    path("expense/category/", ExpenseCategoryList.as_view(), name="expense-category_list"),
+    path("expense/category/<int:pk>/", ExpenseCategoryDetail.as_view(), name="expense-category_detail"),
+    path("income/category/", IncomeCategoryList.as_view(), name="income-category_list"),
+    path("income/category/<int:pk>/", IncomeCategoryDetail.as_view(), name="income-category_detail"),
+    path("account", AccountList.as_view(), name="account_list"),
+    path("account/<int:pk>/", AccountDetail.as_view(), name="account_detail"),
+    path("transfer", TransferList.as_view(), name="transfer-category_list"),
+    path("transfer/<int:pk>/", TransferDetail.as_view(), name="transfer-category_detail"),
+    path("expense", ExpenseList.as_view(), name="expense_list"),
+    path("expense/<int:pk>/", ExpenseDetail.as_view(), name="expense_detail"),
+    path("income", IncomeList.as_view(), name="income_list"),
+    path("income/<int:pk>/", IncomeDetail.as_view(), name="income_detail"),
 ]
--- base/views.py
+++ base/views.py
@@ -1,6 +1,77 @@
 from django.http import JsonResponse
+from rest_framework import generics
+
+from .models import ExpenseCategory, IncomeCategory, Account, Transfer, Expense, Income
+from .serializers import (
+    ExpenseCategorySerializer,
+    IncomeCategorySerializer,
+    AccountSerializer,
+    TransferSerializer,
+    IncomeSerializer,
+    ExpenseSerializer,
+)
 
 
 # Create your views here.
 def hello_world(request):
     return JsonResponse({"data": "Hello world!"})
+
+
+class ExpenseCategoryList(generics.ListCreateAPIView):
+    queryset = ExpenseCategory.objects.all()
+    serializer_class = ExpenseCategorySerializer
+
+
+class ExpenseCategoryDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = ExpenseCategory.objects.all()
+    serializer_class = ExpenseCategorySerializer
+
+
+class IncomeCategoryList(generics.ListCreateAPIView):
+    queryset = IncomeCategory.objects.all()
+    serializer_class = IncomeCategorySerializer
+
+
+class IncomeCategoryDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = IncomeCategory.objects.all()
+    serializer_class = IncomeCategorySerializer
+
+
+class AccountList(generics.ListCreateAPIView):
+    queryset = Account.objects.all()
+    serializer_class = AccountSerializer
+
+
+class AccountDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Account.objects.all()
+    serializer_class = AccountSerializer
+
+
+class TransferList(generics.ListCreateAPIView):
+    queryset = Transfer.objects.all()
+    serializer_class = TransferSerializer
+
+
+class TransferDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Transfer.objects.all()
+    serializer_class = TransferSerializer
+
+
+class ExpenseList(generics.ListCreateAPIView):
+    queryset = Expense.objects.all()
+    serializer_class = ExpenseSerializer
+
+
+class ExpenseDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Expense.objects.all()
+    serializer_class = ExpenseSerializer
+
+
+class IncomeList(generics.ListCreateAPIView):
+    queryset = Income.objects.all()
+    serializer_class = IncomeSerializer
+
+
+class IncomeDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Income.objects.all()
+    serializer_class = IncomeSerializer
